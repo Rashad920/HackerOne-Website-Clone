36da12af8deef8068fe47cb556db646f2bdbbfcf
Rashadul Islam <55145399+Rashad920@users.noreply.github.com>
2021-07-16 21:53:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// const body = document.querySelector("body");
+// const navbar = document.querySelector(".mainMenu");
+// const menuBtn = document.querySelector(".menu_btn");
+// const cancleBtn = document.querySelector(".cancle_btn");
+
+// menuBtn.onclick = () => {
+
+// }
